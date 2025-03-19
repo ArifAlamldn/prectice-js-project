c0b0e88d159ca8ad80e599909b0c15d8cc17fc93
arif alam <arifalamldn@gmail.com>
2025-03-20 01:46:57 +0530

    code

---
+++ OneDrive/Desktop/js30/Counter/index.js
@@ -0,0 +1,14 @@
+let count = 0
+
+const ct = document.querySelector("#count")
+const inc = document.querySelector("#inc")
+ct.innerHTML = `${count}`
+
+const increase =(inc)=>{
+    count += inc
+    ct.innerHTML =`${count}`
+}
+const decrease =()=>{
+    count--
+    ct.innerHTML =`${count}`
+}
